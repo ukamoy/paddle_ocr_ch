da137707703d6e11c351cf47df2190a167f91ecf
ukamoy <uk.amoy@gmail.com>
2026-03-17 09:15:57 -0400

    ocr params optimize

--- main.py
+++ main.py
@@ -17,10 +17,9 @@ async def ocr_api(req: OCRRequest):
             res[url] = await ocr_from_url(url)
         except Exception as e:
             res[url] = {"err": f"ERROR: {e}"}
-    print(res)
+    print("main:",res)
     return {
-        "texts": "\n".join([data["texts"] for data in res.values() if "texts" in data]),
-        "raw":res
+        "texts": "\n\n".join([data["texts"] for data in res.values() if "texts" in data]),
+        "raw": res
     }
-
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
--- ocr.py
+++ ocr.py
@@ -7,7 +7,6 @@ import asyncio
 from concurrent.futures import ThreadPoolExecutor
 
 ocr_executor = ThreadPoolExecutor(max_workers=1)
-
 async def run_ocr(img):
     loop = asyncio.get_running_loop()
     return await loop.run_in_executor(
@@ -32,9 +31,13 @@ ocr = PaddleOCR(
     use_gpu=False,
     lang='ch',                # 语言：中文
     use_angle_cls=False,      # ARM 必关, 禁用方向分类
-    det_limit_side_len=640,
-    det_db_unclip_ratio=1.2,
-    rec_batch_num=2,
+    # v='PP-OCRv5',
+    # text_detection_model_dir="models/PP-OCRv5_server_det_infer",
+    # text_recognition_model_dir="models/PP-OCRv5_server_rec_infer",
+    # text_classifier_model_dir="models/PP-LCNet_x1_0_textline_ori_infer",
+    det_limit_side_len=960,
+    det_db_unclip_ratio=2.8, # 数值加大后对英文空格识别有效
+    rec_batch_num=4,
     cpu_threads=2,
     max_batch_size=1,
     enable_mkldnn=False,
@@ -44,6 +47,8 @@ ocr = PaddleOCR(
 )
 
 def parse_ocr_result(result):
+    if not result or result[0] is None:
+        return []
     lines = []
     for box, (text, score) in result[0]:
         lines.append({
@@ -62,9 +67,10 @@ async def ocr_from_url(url: str) -> str:
     img_bytes = await fetch_image(url)
     img = Image.open(BytesIO(img_bytes)).convert("RGB")
     img = np.array(img)  # 转成 numpy.ndarray
-
+    print(f"Image downloaded, size={len(img_bytes)} bytes")
     result = await run_ocr(img)
     data = parse_ocr_result(result)
+    print(f"OCR parsed lines count={len(data)}")
     return {
         "texts": "\n".join([line["text"] for line in data]),
         "raw": data
